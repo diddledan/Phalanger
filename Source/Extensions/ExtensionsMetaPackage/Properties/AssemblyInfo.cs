98d2e64ddd05014637e10dab2d8543eb4a205bd1
Daniel Llewellyn <Daniel Llewellyn>
2016-01-22 17:00:35 +0000

    rearrange project files
    - move .csproj files away
    - reference new location of .csproj files in Phalanger.sln
    - create new solution for Nuget projects
    - add new .xproj-based projects to nuget solution

---
+++ Source/Extensions/ExtensionsMetaPackage/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ExtensionsMetaPackage")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExtensionsMetaPackage")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6d808318-8246-46ad-8f9d-792432f17d77")]
